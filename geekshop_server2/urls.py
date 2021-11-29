6b29d61f1b1689192ba602c38ffcbc94f6db1fde
KanZaria777 <kanzaria777@gmail.com>
2021-11-30 00:24:12 +0300

    HW2

--- geekshop_server2/urls.py
+++ geekshop_server2/urls.py
@@ -20,6 +20,7 @@ from mainapp import views as mainapp
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('products/', mainapp.products, name='products'),
+    path('products/<name>/', mainapp.products, name='product_category'),
     path('contact/', mainapp.contact, name='contact'),
 
     path('admin/', admin.site.urls),
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,10 +4,50 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainapp/index.html')
 
-
-def products(request):
-    return render(request, 'mainapp/products.html')
-
-
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+links_menu = [
+    {'link_name': 'home', 'name': 'Дом'},
+    {'link_name': 'modern', 'name': 'Модерн'},
+    {'link_name': 'office', 'name': 'Офис'},
+    {'link_name': 'classic', 'name': 'Классика'},
+]
+
+
+def products(request, name=None):
+    context = {
+        'links_menu': links_menu,
+    }
+    return render(request, 'mainapp/products.html', context)
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
